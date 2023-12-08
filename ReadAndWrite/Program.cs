de97d2d930a1e907d425b5fabb10b79028458302
Man0j22 <97286304+Man0j22@users.noreply.github.com>
2023-12-08 15:46:35 +0530

    Add files via upload

---
+++ ReadAndWrite/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter ur name: ");
+
+        string name = Console.ReadLine();
+        string name1 = Console.ReadLine();
+
+        Console.WriteLine("Hello " + name +" "+ name1); //concatenation
+
+        Console.WriteLine("Hello {0}, {1}", name, name1); //place holder syntax
+
+    }
+}
+//note : c# is case-sensitive language
